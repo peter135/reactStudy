5f2ebdca37fa1202579d5d6be8bcf5d5bf5a2391
wangjian <15995249412@163.com>
2022-06-15 16:26:43 +0800

    【react hooks 学习】

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -1,7 +1,22 @@
 import {Counter} from '@/components/hooks/useState/counter.js';
-import GlobalStats_ from '@/components/hooks/useEffect/index.js';
+import RefTutorial from '@/components/hooks/useRef/refTutorial.js';
+import ReducerTutorial from '@/components/hooks/useReducer/reduceTutorial.js';
+
+import CallBackTutorial from '@/components/hooks/UseCallback/CallBackTutorial.js';
+import ContextTutorial from '@/components/hooks/UseContext/ContextTutorial.js';
+import EffectTutorial from '@/components/hooks/useEffect/effectTutorial.js';
+import ImperativeHandle from '@/components/hooks/UseImperativeHandle/ImperativeHandle.js';
+import LayoutEffectTutorial from '@/components/hooks/UseLayoutEffect/LayoutEffectTutorial.js';
+import MemoTutorial from '@/components/hooks/UseMemo/MemoTutorial.js';
 
 export {
     Counter,
-    GlobalStats_
+    RefTutorial,
+    ReducerTutorial,
+    CallBackTutorial,
+    ContextTutorial,
+    EffectTutorial,
+    ImperativeHandle,
+    LayoutEffectTutorial,
+    MemoTutorial
 }
--- src/pages/list/list.page.js
+++ src/pages/list/list.page.js
@@ -2,7 +2,14 @@ import React from 'react';
 import useLocalStorage from 'use-local-storage';
 import styles from './list.module.scss';
 import {Counter} from '@/components/hooks/index';
-
+import {RefTutorial} from '@/components/hooks/index';
+import {ReducerTutorial} from '@/components/hooks/index';
+import {CallBackTutorial} from '@/components/hooks/index';
+import {ContextTutorial} from '@/components/hooks/index';
+import {EffectTutorial} from '@/components/hooks/index';
+import {ImperativeHandle} from '@/components/hooks/index';
+import {LayoutEffectTutorial} from '@/components/hooks/index';
+import {MemoTutorial} from '@/components/hooks/index';
 
 function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -15,11 +22,19 @@ function App() {
 
   return (
     <div className={styles.app} data-theme={theme}>
-      <span>Easy Darkmode and Themes in React</span>
-      <button onClick={switchTheme}>
-        Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
-      </button>
-     <Counter/>
+          <span>Easy Darkmode and Themes in React</span>
+          <button onClick={switchTheme}>
+            Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+          </button>
+           <Counter/>
+           <RefTutorial/>
+           <ReducerTutorial/>
+           <CallBackTutorial/>
+           <ContextTutorial/>
+           <EffectTutorial/>
+           <ImperativeHandle/>
+           <LayoutEffectTutorial/>
+           <MemoTutorial/>
     </div>
   );
 }
