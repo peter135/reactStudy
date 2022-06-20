9a2be77bbfe201c10b187215a8589d6a02b1ca60
wangjian <15995249412@163.com>
2022-06-20 16:09:05 +0800

    【下拉列表】

--- src/pages/list/list.page.js
+++ src/pages/list/list.page.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import useLocalStorage from 'use-local-storage';
 import styles from './list.module.scss';
+
+import DropDown from '@/components/dropdown/index';
+
 import {Counter} from '@/components/hooks/index';
 import {RefTutorial} from '@/components/hooks/index';
 import {ReducerTutorial} from '@/components/hooks/index';
@@ -12,31 +15,48 @@ import {LayoutEffectTutorial} from '@/components/hooks/index';
 import {MemoTutorial} from '@/components/hooks/index';
 
 function App() {
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
-  const switchTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-  }
+  function ThemeSwitch() {
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  
+    const switchTheme = () => {
+      const newTheme = theme === 'light' ? 'dark' : 'light';
+      setTheme(newTheme);
+    }
+  
+    return (
+      <div className={styles.app} data-theme={theme}>
+            <span>Easy Darkmode and Themes in React</span>
+            <button onClick={switchTheme}>
+              Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+            </button>
+      </div>
+    );
+  
+  }  
 
   return (
-    <div className={styles.app} data-theme={theme}>
-          <span>Easy Darkmode and Themes in React</span>
-          <button onClick={switchTheme}>
-            Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
-          </button>
-           {/* <Counter/>
-           <RefTutorial/>
-           <ReducerTutorial/>
-           <CallBackTutorial/>
-           <ContextTutorial/>
-           <EffectTutorial/>
-           <ImperativeHandle/>
-           <LayoutEffectTutorial/>
-           <MemoTutorial/> */}
-    </div>
-  );
+    <>
+      {/* <ThemeSwitch/> */}
+      <DropDown/>
+    </>
+
+  )
+
 }
 
+
 export default App;
+
+
+// hooks
+//    {/* <Counter/>
+//  <RefTutorial/>
+//  <ReducerTutorial/>
+//  <CallBackTutorial/>
+//  <ContextTutorial/>
+//  <EffectTutorial/>
+//  <ImperativeHandle/>
+//  <LayoutEffectTutorial/>
+//  <MemoTutorial/> */}
