ada2500bb4ea4fb213fa31b9b3d9464e9cce37e1
wangjian <15995249412@163.com>
2022-06-28 14:14:45 +0800

    【覆盖bootstrap样式】

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store,history } from './initData'
 import { Main } from './router/index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 ReactDOM.render(
